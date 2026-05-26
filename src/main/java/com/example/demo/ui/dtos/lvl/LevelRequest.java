53a2c86729dd2fa99f506e739782296f9145cf07
Jhon David <escorpio4653@gmail.com>
2026-05-26 13:08:03 +0200

    fix: arreglado swagger en level

---
+++ src/main/java/com/example/demo/ui/dtos/lvl/LevelRequest.java
@@ -0,0 +1,7 @@
+package com.example.demo.ui.dtos.lvl;
+
+public record LevelRequest (
+		int id,
+		String credentialEncripted)
+{
+}
--- src/main/java/com/example/demo/ui/dtos/lvl/LevelResponse.java
+++ src/main/java/com/example/demo/ui/dtos/lvl/LevelResponse.java
@@ -3,7 +3,7 @@ package com.example.demo.ui.dtos.lvl;
 import com.example.demo.domain.entities.Level;
 
 public record LevelResponse(
-        Level level
-
+        Level level,
+        boolean isPassed
 ) {
 }
